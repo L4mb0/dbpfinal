1ad99a2c169bf8bd19de9b2d4705c40b293b6af6
L4mb0 <juan.navarro@utec.edu.pe>
2018-12-06 01:03:41 -0500

    commit con unas correcciones

--- DBPforoMobile/app/src/main/java/com/example/root/dbpforomobile/MainActivity.java
+++ DBPforoMobile/app/src/main/java/com/example/root/dbpforomobile/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Welcome to MySeriesList");
+        setTitle("Bienvenido al Batiforo :D");
     }
 
 
